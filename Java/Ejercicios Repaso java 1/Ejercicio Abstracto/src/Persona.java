968a81fabb72f3e507328adbcf8e77b58ea1d794
David Borja Mateo <dborjam@iesch.org>
2024-03-04 20:55:33 +0100

    Primera Subida

---
+++ Java/Ejercicios Repaso java 1/Ejercicio Abstracto/src/Persona.java
@@ -0,0 +1,35 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Persona {
+    String dni;
+    String nombre;
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "dni='" + dni + '\'' +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
+
+    public Persona(String dni, String nombre) {
+        this.dni = dni;
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
