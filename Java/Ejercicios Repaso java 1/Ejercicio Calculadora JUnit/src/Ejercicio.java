83981d5d53009e3a68f9c3840955e44592d850d7
David Borja Mateo <dborjam@iesch.org>
2024-03-19 19:05:14 +0100

    19/03/2024

---
+++ Java/Ejercicios Repaso java 1/Ejercicio Calculadora JUnit/src/Ejercicio.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Ejercicio {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Ejemplo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+    }
+}
