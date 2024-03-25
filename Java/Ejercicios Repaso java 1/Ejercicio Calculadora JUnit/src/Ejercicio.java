f455de80b01cfc3c6e8b9693d9837bf7adfe93aa
David Borja Mateo <dborjam@iesch.org>
2024-03-25 18:52:58 +0100

    22/03/2024

--- Java/Ejercicios Repaso java 1/Ejercicio Calculadora JUnit/src/Ejercicio.java
+++ Java/Ejercicios Repaso java 1/Ejercicio Calculadora JUnit/src/Ejercicio.java
@@ -1,25 +1,133 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Ejercicio {
+public class Ejercicio extends JFrame implements ActionListener{
+
+    private JButton[] buttonsNumbers;
+    private JButton[] buttonsOperations;
+    private JTextField texto;
+    private String operando1;
+    private String operacion;
+
+    public Ejercicio() {
+
+        //JFrame frame = new JFrame("Calculadora");
+        this.setTitle("Calculadora");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(300,300);
+
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+        panel1.setLayout(new GridLayout (1,1));
+        panel2.setLayout(new GridLayout (4,4));
+        panel3.setLayout(new GridLayout (3,2));
+
+        texto = new JTextField();
+        texto.setSize(150,150);
+        Font font1 = new Font("Calibri", Font.BOLD, 28);
+        texto.setFont(font1);
+        texto.setEditable(false);
+        panel1.add(texto);
+        this.add(panel1,BorderLayout.NORTH);
+
+
+        creaBotones(panel2, this);
+        this.add(panel2,BorderLayout.CENTER);
+
+        creaBotonesOperaciones(panel3,this);
+        this.add(panel3,BorderLayout.EAST);
+
+        /*try {
+            // Set cross-platform Java L&F (also called "Metal")
+            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (UnsupportedLookAndFeelException e) {
+            // handle exception
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }*/
+
+
+    }
+
+    private void creaBotonesOperaciones(JPanel panel3, Ejercicio calculadora) {
+
+        buttonsOperations = new JButton[5];
+        buttonsOperations[0] = new JButton("*");
+        buttonsOperations[1] = new JButton("/");
+        buttonsOperations[2] = new JButton("+");
+        buttonsOperations[3] = new JButton("-");
+        buttonsOperations[4] = new JButton("=");
+
+        for (int i = 0; i < buttonsOperations.length; i++) {
+            panel3.add(buttonsOperations[i]);
+            buttonsOperations[i].addActionListener((ActionListener) calculadora);
+        }
+
+    }
+
+    private void creaBotones(JPanel panel, JFrame ventana) {
+        buttonsNumbers = new JButton[10];
+        for (int i = 9; i >= 0; i--) {
+            buttonsNumbers[i] = new JButton(Integer.toString(i));
+            panel.add(buttonsNumbers[i]);
+            buttonsNumbers[i].addActionListener((ActionListener) ventana);
+        }
+    }
+
+    public void actionPerformed (ActionEvent e){
+        //Obtener el boton presionado:
+        JButton boton = (JButton) e.getSource();
+        String textoDelBoton = boton.getText();
+        System.out.println(textoDelBoton);
+        //Si se presiona un numero, tendré que escribirlo en el TextField
+        if (textoDelBoton.matches("[0-9]")){
+            texto.setText(texto.getText() + textoDelBoton);
+        }
+        //Si se presiona un operador (+-*/) --> Calcular
+        else if (textoDelBoton.matches("[+\\-*/]")) {
+            operando1 = texto.getText();
+            texto.setText(operando1 + textoDelBoton);
+            operacion = textoDelBoton;
+            System.out.println(operando1 + operacion);
+        } else if (textoDelBoton.equals("=")) {
+            //guardarme el numero de la pantalla.
+            String operando2 = texto.getText();
+
+            operando2 = operando2.replace(operando1 + operacion, "");
+            System.out.println(operando2);
+            texto.setText("");
+            int resultado = 0;
+            if (operacion.equals("+")) {
+                resultado = Integer.parseInt(operando1) + Integer.parseInt(operando2);
+                texto.setText("" + resultado);
+            }
+            else if (operacion.equals("-")){
+                resultado = Integer.parseInt(operando1) - Integer.parseInt(operando2);
+                texto.setText("" + resultado);
+            }else  if (operacion.equals("*")){
+                resultado = Integer.parseInt(operando1) * Integer.parseInt(operando2);
+                texto.setText("" + resultado);
+            }else  if (operacion.equals("/")){
+                resultado = Integer.parseInt(operando1) / Integer.parseInt(operando2);
+                texto.setText("" + resultado);
+            }
+
+        }
+    }
+
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Ejemplo");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
-        frame.setLayout(new GridLayout(3, 4));
-        frame.add(new JButton("1"));
-        frame.add(new JButton("2"));
-        frame.add(new JButton("3"));
-        frame.add(new JButton("4"));
-        frame.add(new JButton("5"));
-        frame.add(new JButton("6"));
-        frame.add(new JButton("7"));
-        frame.add(new JButton("8"));
-        frame.add(new JButton("9"));
-        frame.add(new JButton("+"));
-        frame.add(new JButton("-"));
-        frame.add(new JButton("/"));
-        frame.add(new JButton("*"));
-        frame.setVisible(true);
+
+        Ejercicio calculadora = new Ejercicio();
+        calculadora.setVisible(true);
+
     }
 }
